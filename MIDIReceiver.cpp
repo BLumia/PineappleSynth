ec46be1d71f1bc6cc0af5692e6a5dc4e1b7672cc
Chris Xiong <chirs241097@gmail.com>
2016-08-26 08:44:58 +0800

    Add the missing file...

--- MIDIReceiver.cpp
+++ MIDIReceiver.cpp
@@ -1,9 +1,11 @@
 #include "MIDIReceiver.h"
 
+MIDIReceiver* MIDIReceiver::ref=NULL;
+
 void MIDIReceiver::onMessageReceived(IMidiMsg* midiMessage) {
 	IMidiMsg::EStatusMsg status = midiMessage->StatusMsg();
 	// We're only interested in Note On/Off messages (not CC, pitch, etc.)
-	if (status == IMidiMsg::kNoteOn || status == IMidiMsg::kNoteOff) {
+	if (status == IMidiMsg::kNoteOn || status == IMidiMsg::kNoteOff || status == IMidiMsg::kPitchWheel) {
 		mMidiQueue.Add(midiMessage);
 	}
 }
@@ -15,6 +17,14 @@ void MIDIReceiver::advance() {
 		if (midiMessage->mOffset > mOffset) break;
 
 		IMidiMsg::EStatusMsg status = midiMessage->StatusMsg();
+		if (status == IMidiMsg::kPitchWheel)
+		{
+			pbend[midiMessage->Channel()] = midiMessage->PitchWheel();
+			pbChanged();
+			mMidiQueue.Remove();
+			continue;
+		}
+		int channel = midiMessage->Channel();
 		int noteNumber = midiMessage->NoteNumber();
 		int velocity = midiMessage->Velocity();
 		// There are only note on/off messages in the queue, see ::OnMessageReceived
@@ -22,13 +32,13 @@ void MIDIReceiver::advance() {
 			if (mKeyStatus[noteNumber] == false) {
 				mKeyStatus[noteNumber] = true;
 				mNumKeys += 1;
-				noteOn(noteNumber, velocity);
+				noteOn(channel, noteNumber, velocity);
 			}
 		} else {
 			if (mKeyStatus[noteNumber] == true) {
 				mKeyStatus[noteNumber] = false;
 				mNumKeys -= 1;
-				noteOff(noteNumber, velocity);
+				noteOff(channel, noteNumber, velocity);
 			}
 		}
 		mMidiQueue.Remove();
